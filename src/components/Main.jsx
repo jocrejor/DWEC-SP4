d0c9903c806500fef36f249b191bb037b78a2590
Andrea Mendez <andbermen@alu.edu.gva.es>
2024-12-26 17:50:26 +0100

    .

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,36 +5,36 @@ function Main() {
 
   return (
     <>
-        <div class="col-10 padding-zero">
+        <div className="col-10 padding-zero">
             <div>
                 <Header title="Llistat de productes" />
-                <div class="container-fluid">
-                    <div class="row rowFiltres">
-                        <div class="col-4">
-                            <div class="boxFiltre">
+                <div className="container-fluid">
+                    <div className="row rowFiltres">
+                        <div className="col-4">
+                            <div className="boxFiltre">
                                 <label for="name">Nom:</label>
                                 <input type="text" id="name" placeholder="Filtrar per nom"/>
                             </div>
                         </div>
 
-                        <div class="col-4">
-                            <div class="boxFiltre">
+                        <div className="col-4">
+                            <div className="boxFiltre">
                                 <label for="description">Descripció:</label>
                                 <input type="text" id="description" placeholder="Filtrar per descripció"/>
                             </div>
                         </div>
 
-                        <div class="col-4">
-                            <div class="boxFiltre">                
+                        <div className="col-4">
+                            <div className="boxFiltre">                
                                 <label for="sku">SKU:</label>
                                 <input type="text" id="sku" placeholder="Filtrar per SKU"/>                 
                             </div>
                         </div>
                     </div>
 
-                    {/*<!--<div class="row rowFiltres">                           
-                        <div class="col-4">
-                            <div class="boxFiltre">
+                    {/*<!--<div className="row rowFiltres">                           
+                        <div className="col-4">
+                            <div className="boxFiltre">
                                 <label for="lot">Filtrar per Lot/Serial:</label>
                                 <input type="text" id="lot">
                             </div>
@@ -42,12 +42,12 @@ function Main() {
                     </div>-->*/}
 
                     {/**FILTRES BOTÓ */}
-                    <div class="row rowFiltres">
-                        <div class="col-12">
-                            <div class="container-button-filter botonesFiltros"/>
-                        <div>
-                            <button id="netejaFiltros">Esborrar</button>
-                            <button id="filtrarProductes">Filtrar</button>
+                    <div className="row rowFiltres">
+                        <div className="col-12">
+                            <div className="container-button-filter botonesFiltros">
+                                <button id="netejaFiltros">Esborrar</button>
+                                <button id="filtrarProductes">Filtrar</button>
+                            </div>
                         </div>
                     </div>
                 </div>

23c446880448523933be586ebf57fcbbc370fc4f
David <davsermel@gmail.com>
2025-01-20 12:30:06 +0100

    intentar arreglar

--- src/components/Moviments.jsx
+++ src/components/Moviments.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import Header from './header'
+
 import { url,  getData} from '../apiAccess/crud'
 import { Button,Modal,ModalBody,ModalFooter } from 'react-bootstrap';
 

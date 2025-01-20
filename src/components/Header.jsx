80e3af2ad73be5944b32efbda2310c6394309eb1
jocrejor <jocrejor@gmail.com>
2025-01-20 18:59:48 +0100

    arreglar duplicats del Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 
 function Header(props) {
-<<<<<<< HEAD
-    const {title} = props;
-  return (
-    <h1 className="text-center py-4 fs-4 fw-bold m-0 text-white bg-title">{title}</h1>
-=======
+
   const { title } = props;
   return (
     <div>
       <h1 className="text-center py-4 fs-4 fw-bold m-0 text-white bg-title">{title}</h1>
       <p className='position-absolute top-0 end-0 me-4 mt-4 fw-bold fs-5 text-white'><i class="bi bi-person-circle pe-2"></i>Usuari Admin</p>
     </div>
->>>>>>> main
+
   )
 }
 

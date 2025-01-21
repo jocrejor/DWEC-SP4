f968a503a5a2c9a08df2a84fe31313bb49525249
looreenz <lorenzo.cremonese10@gmail.com>
2025-01-21 09:17:06 +0100

    No code changes detected

--- src/components/OrderPickingShipping.jsx
+++ src/components/OrderPickingShipping.jsx
@@ -3,173 +3,278 @@ import { Formik, Form, Field } from "formik";
 import { Button, Table, Modal } from "react-bootstrap";
 import { url, getData, postData, deleteData } from "../apiAccess/crud";
 
+// function OrderPickingShipping() {
+//   const [ships, setShips] = useState([]);
+//   const [show, setShow] = useState(false);
+//   const [lines, setLines] = useState([]);
+//   const [tipoModal, setTipoModal] = useState();
+//   const [shipToShow, setShipToShow] = useState();
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const orderPicking = await getData(url, "OrderPickingshipping");
+//         setShips(orderPicking);
+//         const lineas = await getData(url, "OrderLineShipping");
+//         setLines(lineas);
+//       } catch (error) {
+//         console.error("Error fetching data:", error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   const handleModal = () => {
+//     setShow(!show);
+//   };
+
+//   const eliminarPicking = (id) => {
+//     deleteData(url, "OrderPickingshipping", id);
+//     const newShips = ships.filter((ship) => ship.id !== id);
+//     setShips(newShips);
+//   };
+
+//   const createOrderPicking = (values) => {
+//     values.create_date = new Date(
+//       new Date().getFullYear(),
+//       new Date().getMonth(),
+//       new Date().getDate()
+//     );
+//     postData(url, "OrderPickingshipping", values);
+//   };
+
+//   const showShip = (shipToShow) => {
+//     if (shipToShow) {
+//       const selectedShip = ships.find((ship) => ship.id == shipToShow);
+//       return selectedShip.pickingList.map((pick) => (
+//         <tr key={pick}>
+//           <td>{pick}</td>
+//         </tr>
+//       ));
+//     }
+//   };
+
+//   return (
+//     <>
+//       <h1>Order Picking Ships</h1>
+//       <Button
+//         variant="success"
+//         onClick={() => {
+//           setTipoModal("Alta");
+//           handleModal();
+//         }}
+//       >
+//         Alta Orden
+//       </Button>
+
+//       <Table striped bordered hover>
+//         <thead>
+//           <tr>
+//             <th>Id</th>
+//             <th>Data Creació</th>
+//             <th>Ver</th>
+//             <th>Eliminar</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {ships.map((ship) => {
+//             return (
+//               <tr key={ship.id}>
+//                 <td>{ship.id}</td>
+//                 <td>{ship.create_date}</td>
+//                 <td>
+//                   <Button
+//                     onClick={() => {
+//                       setTipoModal("Ver");
+//                       setShipToShow(ship.id);
+//                       handleModal();
+//                     }}
+//                   >
+//                     Ver
+//                   </Button>
+//                 </td>
+//                 <td>
+//                   <Button
+//                     variant="danger"
+//                     onClick={() => {
+//                       eliminarPicking(ship.id);
+//                     }}
+//                   >
+//                     Eliminar
+//                   </Button>
+//                 </td>
+//               </tr>
+//             );
+//           })}
+//         </tbody>
+//       </Table>
+
+//       <Modal show={show} onHide={handleModal}>
+//         <Modal.Header closeButton>
+//           <Modal.Title>{tipoModal} Order Picking Ship</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>
+//           <Formik
+//             initialValues={{
+//               pickingList: [],
+//             }}
+//             onSubmit={(values) => {
+//               console.log(lines);
+//               console.log(values);
+//               createOrderPicking(values);
+//               handleModal();
+//             }}
+//           >
+//             <Form>
+//               <Table>
+//                 <thead>
+//                   <tr>
+//                     <th>Sel.</th>
+//                     <th>Prducte</th>
+//                     <th>Quantitat</th>
+//                     <th>Id Ordre</th>
+//                   </tr>
+//                 </thead>
+//                 <tbody>
+//                   {tipoModal === "Alta"
+//                     ? lines.map((line) => (
+//                         <tr key={line.id}>
+//                           <td>
+//                             <Field
+//                               type="checkbox"
+//                               name="pickingList"
+//                               value={line.id}
+//                             />
+//                           </td>
+//                           <td>{line.product_id}</td>
+//                           <td>{line.quantity}</td>
+//                           <td>{line.shipping_order_id}</td>
+//                         </tr>
+//                       ))
+//                     : showShip(shipToShow)}
+//                 </tbody>
+//               </Table>
+//               <div>
+//                 <Button variant="secondary" onClick={handleModal}>
+//                   Close
+//                 </Button>
+//                 {tipoModal === "Alta" && (
+//                   <Button variant="success" type="submit">
+//                     Alta
+//                   </Button>
+//                 )}
+//               </div>
+//             </Form>
+//           </Formik>
+//         </Modal.Body>
+//       </Modal>
+//     </>
+//   );
+// }
+
 function OrderPickingShipping() {
-  const [ships, setShips] = useState([]);
-  const [show, setShow] = useState(false);
-  const [lines, setLines] = useState([]);
-  const [tipoModal, setTipoModal] = useState();
-  const [shipToShow, setShipToShow] = useState();
+  const [orderPickingShipping, setOrderPickingShipping] = useState([]);
+  const [orderShipping, setOrderShipping] = useState([]);
+  const [orderLineShipping, setOrderLineShipping] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [temporalPickings, setTemporalPickings] = useState([]);
+  const [spaces, setSpaces] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [tipoModal, setTipoModal] = useState("Alta");
+
+  // const [orderSelected, setOrderSelected] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const orderPicking = await getData(url, "OrderPickingshipping");
-        setShips(orderPicking);
-        const lineas = await getData(url, "OrderLineShipping");
-        setLines(lineas);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+      const fetchData = async () => {
+          const orderPicking = await getData(url, "OrderPickingShipping");
+          setOrderPickingShipping(orderPicking);
 
-    fetchData();
+          const orderShipping = await getData(url, "OrderShipping");
+          setOrderShipping(orderShipping);
+
+          const orderLine = await getData(url, "OrderLineShipping");
+          setOrderLineShipping(orderLine);
+
+          const product = await getData(url, "Product");
+          setProducts(product);
+
+          const spaces = await getData(url, "Space");
+          setSpaces(spaces);
+
+          //recorrer orden Shipping pendent (desempaquetada)
+          const orderPendent = orderShipping.filter((order) => order.orderShipping_status_id === "ceba");
+          
+          const tempPickings = [];
+          orderPendent.map((order) => {
+              //recorrer line Shipping de cada orden shipping
+              const lines = orderLine.filter((line) => line.order_shipping_id === order.id);
+              //obtindre product.name, product.quantitat, product.space
+              lines.forEach((line) => {
+                  const space = spaces.find((space) => space.product_id === line.product_id);
+                  if (space) {
+                      console.log(order.id, line.id, line.product_id, line.quantity_received, space.storage_id, space.street_id, space.selft_id, space.id);
+                      const objTemporal = {   
+                          order_shipping_id: order.id,
+                          order_line_shipping_id: line.id,
+                          product_id: line.product_id,
+                          quantity_received: line.quantity_received,
+                          storage_id: space.storage_id,
+                          street_id: space.street_id,
+                          selft_id: space.selft_id,
+                          space_id: space.id
+                      }
+                      tempPickings.push(objTemporal);
+                      setTemporalPickings(tempPickings);
+                  }
+              });
+          });
+      };
+      fetchData();
   }, []);
 
-  const handleModal = () => {
-    setShow(!show);
-  };
 
-  const eliminarPicking = (id) => {
-    deleteData(url, "OrderPickingshipping", id);
-    const newShips = ships.filter((ship) => ship.id !== id);
-    setShips(newShips);
-  };
-
-  const createOrderPicking = (values) => {
-    values.create_date = new Date(
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      new Date().getDate()
-    );
-    postData(url, "OrderPickingshipping", values);
-  };
-
-  const showShip = (shipToShow) => {
-    if (shipToShow) {
-      const selectedShip = ships.find((ship) => ship.id == shipToShow);
-      return selectedShip.pickingList.map((pick) => (
-        <tr key={pick}>
-          <td>{pick}</td>
-        </tr>
-      ));
-    }
-  };
+  const canviEstatModal = () => {
+      setShowModal(!showModal);
+  }
 
   return (
-    <>
-      <h1>Order Picking Ships</h1>
-      <Button
-        variant="success"
-        onClick={() => {
-          setTipoModal("Alta");
-          handleModal();
-        }}
-      >
-        Alta Orden
-      </Button>
+      <>
+          <div>
+              <h2>Llistat Order Shipping</h2>
+          </div>
+          <Button variant="success" onClick={() => {
+              canviEstatModal();
+              setTipoModal("Alta")
+          }}>Crear Order Picking</Button>
 
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Data Creació</th>
-            <th>Ver</th>
-            <th>Eliminar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {ships.map((ship) => {
-            return (
-              <tr key={ship.id}>
-                <td>{ship.id}</td>
-                <td>{ship.create_date}</td>
-                <td>
-                  <Button
-                    onClick={() => {
-                      setTipoModal("Ver");
-                      setShipToShow(ship.id);
-                      handleModal();
-                    }}
-                  >
-                    Ver
-                  </Button>
-                </td>
-                <td>
-                  <Button
-                    variant="danger"
-                    onClick={() => {
-                      eliminarPicking(ship.id);
-                    }}
-                  >
-                    Eliminar
-                  </Button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
-
-      <Modal show={show} onHide={handleModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>{tipoModal} Order Picking Ship</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Formik
-            initialValues={{
-              pickingList: [],
-            }}
-            onSubmit={(values) => {
-              console.log(lines);
-              console.log(values);
-              createOrderPicking(values);
-              handleModal();
-            }}
-          >
-            <Form>
-              <Table>
-                <thead>
+          <Table striped bordered hover>
+              <thead>
                   <tr>
-                    <th>Sel.</th>
-                    <th>Prducte</th>
-                    <th>Quantitat</th>
-                    <th>Id Ordre</th>
+                      <th>ID Order</th>
+                      <th>Producte</th>
+                      <th>Quantitat</th>
+                      <th>Magatzem / Carrer / Estantería / Espai</th>
+                      <th></th>
                   </tr>
-                </thead>
-                <tbody>
-                  {tipoModal === "Alta"
-                    ? lines.map((line) => (
-                        <tr key={line.id}>
-                          <td>
-                            <Field
-                              type="checkbox"
-                              name="pickingList"
-                              value={line.id}
-                            />
-                          </td>
-                          <td>{line.product_id}</td>
-                          <td>{line.quantity}</td>
-                          <td>{line.shipping_order_id}</td>
-                        </tr>
-                      ))
-                    : showShip(shipToShow)}
-                </tbody>
-              </Table>
-              <div>
-                <Button variant="secondary" onClick={handleModal}>
-                  Close
-                </Button>
-                {tipoModal === "Alta" && (
-                  <Button variant="success" type="submit">
-                    Alta
-                  </Button>
-                )}
-              </div>
-            </Form>
-          </Formik>
-        </Modal.Body>
-      </Modal>
-    </>
+              </thead>
+
+              <tbody>
+                  {temporalPickings.map(temporalPicking => {
+                      const product = products.find(p => p.id === temporalPicking.product_id);
+                      return (
+                          <tr key={temporalPicking.order_shipping_id}>
+                              <td>{temporalPicking.order_shipping_id}</td>
+                              <td>{product.name}</td>
+                              <td>{temporalPicking.quantity_received}</td>
+                              <td>{temporalPicking.storage_id} / {temporalPicking.street_id} / {temporalPicking.selft_id} / {temporalPicking.space_id}</td>
+                              <td><input type="checkbox" /></td>
+                          </tr>
+                      );
+                  })}
+              </tbody>
+          </Table>
+      </>
   );
 }
 

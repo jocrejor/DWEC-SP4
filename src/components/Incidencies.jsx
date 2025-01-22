973fe5fa6fd76239555942d4080cc1a84aa5f255
Hilariano <hilariano1@gmail.com>
2025-01-22 17:28:17 +0100

    Falta botó de resoldre i agarrar els camps correctes de incidència (Crespo té que explicar-me com funciona)

--- src/components/Incidencies.jsx
+++ src/components/Incidencies.jsx
@@ -6,8 +6,16 @@ import { Button, Modal } from 'react-bootstrap';
 import Header from './Header'
 
 const IncidenciaSchema = Yup.object().shape({
-    quantity_received: Yup.number().required('Tens que introduïr una cantitat vàlida').positive().integer(),
-    description: Yup.string().min(4, 'Valor mínim de 4 caracters.').max(200, 'El valor màxim és de 60 caracters'),
+    //Aso es té que llevar quan estiga ben fet, es soles un formulari de alta de prova perque Crespo no m'ha explicat on és fa l'alta i quins camps tinc que posar
+    product:            Yup.string().min(4, 'Valor mínim de 4 caracters.').max(50, 'El valor màxim és de 60 caracters').required('Valor requerit'),
+    supplier:           Yup.string().min(1, 'Valor mínim de 1 caracter.').max(50, 'El valor màxim és de 60 caracters').required('Valor requerit'),
+    operator:           Yup.string().min(1, 'Valor mínim de 1 caracter.').max(50, 'El valor màxim és de 60 caracters').required('Valor requerit'),
+    quantity_ordered:   Yup.number().positive().integer().required('Tens que introduïr una cantitat vàlida'),
+    created_at:         Yup.date().min('2024/01/22', 'No pot ser abans del 22 de gener del 2024').max('2025/01/23').required("Introdueïx una data vàlida", 'No pot ser després del 23 de gener del 2025'),
+
+    //Estos dos camos eb teoria están be per a quan modifiquem la incidència
+    quantity_received: Yup.number().positive().integer().required('Tens que introduïr una cantitat vàlida'),
+    description: Yup.string().min(4, 'Valor mínim de 4 caracters.').max(200, 'El valor màxim és de 60 caracters')
 })
 
 function Incidencies() {
@@ -17,7 +25,15 @@ function Incidencies() {
     const [orderlineStatus, setOrderlineStatus]     = useState([])
     const [showModal, setShowModal]                 = useState(false)
     const [tipoModal, setTipoModal]                 = useState("Crear")
-    const [valorsInicials, setValorsInicials]       = useState({ date_creation: '', description: '', name: ''})
+    const [valorsInicials, setValorsInicials]       = useState({
+        product: '',
+        quantity_received: '',
+        description: '',
+        supplier: '',
+        operator: '',
+        quantity_ordered: '',
+        created_at: ''
+    });
 
     useEffect(() => {      
         (async () => {
@@ -65,14 +81,14 @@ const getStatusName = (statusId) => {
     <Button variant='success' onClick={()=>{canviEstatModal(); setTipoModal("Crear")}}>Llistat ordres de recepció</Button>
         <table>
             <tr>
-            <th>Data de creació</th>
-            <th>Descripció</th>
-            <th>Producte</th>
-            <th>Unitats demanades</th>
-            <th>Unitats rebudes</th>
-            <th>Estat</th>
-            <th>Modificar</th>
-            <th>Eliminar</th>
+                <th>Data de creació</th>
+                <th>Descripció</th>
+                <th>Producte</th>
+                <th>Unitats demanades</th>
+                <th>Unitats rebudes</th>
+                <th>Estat</th>
+                <th>Modificar</th>
+                <th>Eliminar</th>
             </tr>
             {(incidents.length == 0)?
             <div>No hi han articles</div>
@@ -93,13 +109,13 @@ const getStatusName = (statusId) => {
 
       <Modal show={showModal} onHide={canviEstatModal}>
         <Modal.Header closeButton >
-          <Modal.Title>{tipoModal} Producte</Modal.Title>
+          <Modal.Title>{tipoModal} Incidència</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
           
       <Formik
-            initialValues= {(tipoModal==='Modificar'?valorsInicials: {quantity_received: '', description: '' })}
+            initialValues= {(tipoModal==='Modificar'?valorsInicials: {product: '', quantity_received: '', description: '', supplier: '', operator: '', quantity_ordered: '', created_at: '', orderReception_id: '', })}
             validationSchema={IncidenciaSchema}
             onSubmit={values => {
                 console.log(values)
@@ -126,35 +142,36 @@ const getStatusName = (statusId) => {
 
                     value={values.orderReception_id}
                 />
-                {errors.name && touched.name ? <div>{errors.name}</div> : null}
+                {errors.orderReception_id && touched.orderReception_id ? <div>{errors.orderReception_id}</div> : null}
             </div>
             {/*Data creació*/}
             <div>
-                <label htmlFor='data_creacio'>Data creació</label>
+                <label htmlFor='created_at'>Data creació</label>
                 <Field
-                    type="text" 
-                    name="data_creacio"
+                    type="date" 
+                    name="created_at"
                     placeholder="Data de creació"
                     autoComplete="off"
-                    disabled
+                    disabled={tipoModal === "Modificar"}
 
                     value={values.created_at}
                 />
-                {errors.name && touched.name ? <div>{errors.name}</div> : null}
+                {errors.created_at && touched.created_at ? <div>{errors.created_at}</div> : null}
             </div>
             {/*Producte*/}
             <div>
-                <label htmlFor='name'>Producte</label>
+                <label htmlFor='product'>Producte</label>
                 <Field
                     type="text" 
                     name="product"
                     placeholder="Nom del producte"
                     autoComplete="off"
-                    disabled
+                    disabled={tipoModal === "Modificar"}
 
-                    value={getProductName(values.product)}
+                    value={tipoModal === "Modificar" ? getProductName(values.product) : values.product}
                 />
-                {errors.name && touched.name ? <div>{errors.name}</div> : null}
+                {/*{errors.getProductName(values.product) && touched.getProductName(values.product) ? <div>{errors.getProductName(values.product)}</div> : null}*/}
+                {errors.product && touched.product ? <div>{errors.product}</div> : null}
             </div>
             {/*Proveïdor*/}
             <div>
@@ -164,11 +181,11 @@ const getStatusName = (statusId) => {
                     name="supplier"
                     placeholder="Nom del proveïdor"
                     autoComplete="off"
-                    disabled
+                    disabled={tipoModal === "Modificar"}
 
                     value={values.supplier}
                 />
-                {errors.name && touched.name ? <div>{errors.name}</div> : null}
+                {errors.supplier && touched.supplier ? <div>{errors.supplier}</div> : null}
             </div>
             {/*Operari*/}
             <div>
@@ -178,11 +195,11 @@ const getStatusName = (statusId) => {
                     name="operator"
                     placeholder="Operari"
                     autoComplete="off"
-                    disabled
+                    disabled={tipoModal === "Modificar"}
 
                     value={values.operator}
                 />
-                {errors.name && touched.name ? <div>{errors.name}</div> : null}
+                {errors.operator && touched.operator ? <div>{errors.operator}</div> : null}
             </div>
             {/*Quantitat demanada*/}
             <div>
@@ -192,11 +209,11 @@ const getStatusName = (statusId) => {
                     name="quantity_ordered"
                     placeholder="Quantiat demanada"
                     autoComplete="off"
-                    disabled
+                    disabled={tipoModal === "Modificar"}
 
                     value={values.quantity_ordered}
                 />
-                {errors.name && touched.name ? <div>{errors.name}</div> : null}
+                {errors.quantity_ordered && touched.quantity_ordered ? <div>{errors.quantity_ordered}</div> : null}
             </div>
             {/*Quantitat rebuda*/}
             <div>
@@ -209,7 +226,7 @@ const getStatusName = (statusId) => {
 
                     value={values.quantity_received}
                 />
-                {errors.name && touched.name ? <div>{errors.name}</div> : null}
+                {errors.quantity_received && touched.quantity_received ? <div>{errors.quantity_received}</div> : null}
             </div>
             {/*Em senc atacat -- Descripcio*/}
             <div>

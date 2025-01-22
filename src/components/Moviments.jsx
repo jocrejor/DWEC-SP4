2b7b156efed23c2ddd20e6949bc5c0636c383f6b
David <davsermel@gmail.com>
2025-01-22 09:49:05 +0100

    acabat moviments

--- src/components/Moviments.jsx
+++ src/components/Moviments.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Header from "./Header"
+import Filtres from "./Filtres"
 import { url,  getData} from '../apiAccess/crud'
 import { Button,Modal,ModalBody,ModalFooter } from 'react-bootstrap';
 
@@ -27,47 +28,82 @@ useEffect(() => {
   return (
     <>
       <div>
-      <Header  title="Moviments" />
-        <table style={{ marginLeft: 170, marginTop: 120}}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>ID del Producte</th>
-              <th>Magatzem</th>
-              <th>Carrer</th>
-              <th>Estanteria</th>
-              <th>Espai</th>
-              <th>Quantitat</th>
-              <th>Data</th>
-              <th>Operari</th>
-              <th>Origen</th>
-              <th>Accions</th>
-            </tr>
-          </thead>
-          <tbody>
+   <Header  title="Moviments" />
 
-            {moviments.map((valors) => (
-              <tr key={valors.id}>
-                <td>{valors.id}</td>
-                <td>{valors.product_id}</td>
-                <td>{valors.storage_id}</td>
-                <td>{valors.street_id}</td>
-                <td>{valors.shelf_id}</td>
-                <td>{valors.space_id}</td>
-                <td>{valors.quantity}</td>
-                <td>{valors.date}</td>
-                <td>{valors.operator_id}</td>
-                <td>{valors.orgin}</td>
-                <td>
-          <Button variant="primary"  style={{ cursor: 'pointer' }}
-                    onClick={() => handleShow(valors)} >Visualizar </Button>
-                </td>
+<Filtres></Filtres>
+<div className="table-responsive " style={{ marginTop: 70 }}>
+  <table className="table table-bordered">
+    <thead className="thead-light">
+      <tr>
+        <th className="d-none d-md-table-cell">ID</th>
+        <th className="d-none d-md-table-cell">Producte</th>
+        <th className="d-none d-md-table-cell">Magatzem</th>
+        <th className="d-none d-md-table-cell">Carrer</th>
+        <th className="d-none d-md-table-cell">Estanteria</th>
+        <th className="d-none d-md-table-cell">Espai</th>
+        <th className="d-none d-md-table-cell">Quantitat</th>
+        <th className="d-none d-md-table-cell">Data</th>
+        <th className="d-none d-md-table-cell">Operari</th>
+        <th className="d-none d-md-table-cell">Origen</th>
+        <th className="d-none d-md-table-cell">Accion</th>
+      </tr>
+    </thead>
+    <tbody>
+      {moviments.map((valors) => (
+        <tr 
+          key={valors.id} 
+          className="d-block d-md-table-row mb-3" 
+          style={{ border: "1px solid #dee2e6" }}
+        >
+          <td className="d-block d-md-table-cell">
+            <strong className="d-sm-inline d-md-none">ID:</strong> {valors.id}
+          </td>
+          <td className="d-block d-md-table-cell">
+            <strong className="d-sm-inline d-md-none">Producte:</strong> {valors.product_id}
+          </td>
+          <td className="d-block d-md-table-cell">
+            <strong className="d-sm-inline d-md-none">Magatzem:</strong> {valors.storage_id}
+          </td>
+          <td className="d-block d-md-table-cell">
+            <strong className="d-sm-inline d-md-none">Carrer:</strong> {valors.street_id}
+          </td>
+          <td className="d-block d-md-table-cell">
+            <strong className="d-sm-inline d-md-none">Estanteria:</strong> {valors.shelf_id}
+          </td>
+          <td className="d-block d-md-table-cell">
+            <strong className="d-sm-inline d-md-none">Espai:</strong> {valors.space_id}
+          </td>
+          <td className="d-block d-md-table-cell">
+            <strong className="d-sm-inline d-md-none">Quantitat:</strong> {valors.quantity}
+          </td>
+          <td className="d-block d-md-table-cell">
+            <strong className="d-sm-inline d-md-none">Data:</strong> {valors.date}
+          </td>
+          <td className="d-block d-md-table-cell">
+            <strong className="d-sm-inline d-md-none">Operari:</strong> {valors.operator_id}
+          </td>
+          <td className="d-block d-md-table-cell ">
+            <strong className="d-sm-inline d-md-none ">Origen:</strong> {valors.orgin}
+          </td>
+          <td className="d-block d-md-table-cell">
+            <strong className="d-sm-inline d-md-none">Accion:</strong>
+            <Button
+              variant="primary"
+              style={{ cursor: 'pointer' }}
+              onClick={() => handleShow(valors)}
+            >
+              Visualizar
+            </Button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
 
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+
+</div>
+
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Detalls del Moviment</Modal.Title>

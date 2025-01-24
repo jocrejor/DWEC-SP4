fb6346b8ca4fe2dd2841f64774970b0a3b38b205
jocrejor <jocrejor@gmail.com>
2025-01-24 08:46:01 +0100

    Estats d'ordre en dataTables

--- src/components/EstatsOrdre.jsx
+++ src/components/EstatsOrdre.jsx
@@ -1,29 +1,56 @@
-import React, {useState} from 'react'
-import EstatsOrdreList from './EstatsOrdreList'
-import EstatsOrdreAlta from './EstatsOrdreAlta';
-import EstatsOrdreMod from './EstatsOrdreMod';
+import {useState, useEffect} from 'react'
+import DataTable from 'datatables.net-react';
+import DT from 'datatables.net-dt';
+import language from 'datatables.net-plugins/i18n/es-ES.mjs'
+import Header from './Header';
+import {url,getData} from '../apiAccess/crud'
+
 
  function EstatsOrdre() {
-    
+
+    const columnesDataTAbles =  [{data: 'id'}, {data: 'name'}]
     const [estat, setEstat] = useState('Lista');
 
-   const canviEstat =(valor)=>{
-        console.log("dins" . valor)
-        setEstat(valor)
-   }
   
-      {switch (estat){
-        case 'Lista':
-            return ( <EstatsOrdreList  action={canviEstat} />)
-        case 'Alta':
-            return ( <EstatsOrdreAlta action={canviEstat}/>)
-        case 'Modificar':
-            return ( <EstatsOrdreMod action={canviEstat}/>)    
-        default:
-            return ( <EstatsOrdreList  action={canviEstat} />)
-      }    
-      
-}
+    useEffect(() => {
+
+        const inicialitza = async () => {
+            const data = await getData(url, "OrderReception_Status")
+            setEstat(data)
+          }
+          inicialitza()
+          console.log(estat)
+     }, [])
+    
+
+     DataTable.use(DT);
+  
+return (
+    <>
+    <Header title="Estats Ordre" />
+
+    <DataTable 
+        columns = {columnesDataTAbles}
+        data={estat} 
+        className="display"
+        options={{
+                language: language,
+                info: false,
+                responsive: true,
+                select: true,
+            }}
+    >
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+        </tr>
+    </thead>
+</DataTable>
+    
+    
+    </>
+)
 
  }
  export default EstatsOrdre

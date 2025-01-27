4da8ecd7b916eb531813852a16f7b6d5e5662433
Andrea Mendez <andbermen@alu.edu.gva.es>
2025-01-27 12:10:51 +0100

    Setting active options in the lateral menu.

--- src/components/Lateral.jsx
+++ src/components/Lateral.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import logo from '../assets/logo_footer2.png';
 import '../App'
 import Nav from 'react-bootstrap/Nav';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
 function Lateral() {
+    const [activeOption, setActiveOption] = useState();
+    const liActive     = 'list-group-item px-0 opacity-75 activo';
+    const liInactive   = 'list-group-item fondo-azul-claro';
+    const linkActive   = 'text-decoration-none d-block';
+    const linkInactive = 'text-decoration-none text-white d-block';
+
     return (
         <>
         <div className="col-12 col-xl-2 p-0 fondo-azul">
@@ -17,10 +25,10 @@ function Lateral() {
                         <li className="list-group-item py-3 px-1 text-white fondo-azul no-hover"><i className="bi bi-house-fill px-1 text-white"></i>Administració</li>
                         <li className="list-group-item fondo-azul-claro"><a className="text-decoration-none text-white d-block" href="/usuaris">Usuaris</a></li>
                         <li className="list-group-item fondo-azul-claro"><a className="text-decoration-none text-white d-block" href="/rols">Rols</a></li>
-                        <li className="list-group-item fondo-azul-claro"><a className="text-decoration-none text-white d-block" href="/dadesGeografiques">Dades geogràfiques</a></li>
-                        <li className="list-group-item px-0 opacity-75 activo"><a className="text-decoration-none d-block" href="/transportistes"><i className="bi bi-caret-right-fill pe-1"></i>Transportistes</a></li>
+                        <li className={activeOption == 'DadesGeografiques' ? liActive : liInactive}><Link className={activeOption == 'DadesGeografiques' ? linkActive : linkInactive} onClick={()=>{setActiveOption('DadesGeografiques'); console.log(activeOption)}} to="/dadesGeografiques">{activeOption == 'DadesGeografiques' ? <i className="bi bi-caret-right-fill pe-1"></i> : null}Dades geogràfiques</Link></li>
+                        <li className={activeOption == 'Transportistes' ? liActive : liInactive}><Link className={activeOption == 'Transportistes' ? 'text-decoration-none d-block' : 'text-decoration-none text-white d-block'}  onClick={()=>{setActiveOption('Transportistes')}} to="/transportistes">{activeOption == 'Transportistes' ? <i className="bi bi-caret-right-fill pe-1"></i> : null}Transportistes</Link></li>
                         <li className="list-group-item py-3 px-1 text-white fondo-azul border-top no-hover"><i className="bi bi-send px-1 text-white"></i>Enviament</li>
-                        <li className="list-group-item fondo-azul-claro"><a className="text-decoration-none text-white d-block" href="/clients">Clients</a></li>
+                        <li className={activeOption == 'Clients' ? liActive: liInactive}><Link className={activeOption == 'Clients' ? 'text-decoration-none d-block' : 'text-decoration-none text-white d-block'} onClick={()=>{setActiveOption('Clients'); console.log(activeOption)}} to="/clients">{activeOption == 'Clients' ? <i className="bi bi-caret-right-fill pe-1"></i> : null}Clients</Link></li>
                         <li className="list-group-item fondo-azul-claro"><a className="text-decoration-none text-white d-block" href="/ordesEnviament">Ordres d'enviament <span className="badge rounded-pill bg-danger px-3 ms-2 text-white">9</span></a></li>
                         <li className="list-group-item py-3 px-1 text-white fondo-azul border-top no-hover"><i className="bi bi-inboxes px-1 text-white"></i>Recepció</li>
                         <li className="list-group-item fondo-azul-claro"><a className="text-decoration-none text-white d-block" href="/proveidors">Proveïdors</a></li>

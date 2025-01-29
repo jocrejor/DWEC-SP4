4bd7c08bc6eef56ef1f88828b50ebb1f33594766
paula <paula@gmail.com>
2025-01-29 16:38:13 +0100

    visualitzar order picking

--- src/components/OrderPickingReception.jsx
+++ src/components/OrderPickingReception.jsx
@@ -262,6 +262,7 @@ function OrderPickingReception() {
                                     <Table striped bordered hover>
                                         <thead>
                                             <tr>
+                                                <th></th>
                                                 <th>Producte</th>
                                                 <th>Quantitat</th>
                                                 <th>Magatzem / Carrer / Estantería / Espai</th>
@@ -269,7 +270,21 @@ function OrderPickingReception() {
                                         </thead>
 
                                         <tbody>
-                                            
+                                            {orderPickingReception.find(order => order.id === orderVisualitzar)?.productos.map(producto => {
+                                                const product = products.find(p => p.id === producto.product_id);
+                                                const space = spaces.find(s => s.product_id === producto.product_id);
+                                                
+                                                return (
+                                                    <tr key={producto.product_id}>
+                                                        <td> </td>
+                                                        <td>{product.name}</td>
+                                                        <td>{space.quantity}</td>
+                                                        <td>
+                                                           {space.storage_id} / {space.street_id} / {space.selft_id} / {space.id}
+                                                        </td>
+                                                    </tr>
+                                                );
+                                            })}
                                         </tbody>
                                     </Table>
                                     <Button variant="success" onClick={() => {
